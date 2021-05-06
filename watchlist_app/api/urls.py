16611183db0038d534141663955be008a849bec2
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-05-06 22:34:15 +0300

    changed urls to class based view urls

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from watchlist_app.api.views import *
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>', movie_details, name='movie-details')
+    path('list/', MovieListView.as_view(), name='movie-list'),
+    path('<int:pk>', MovieDetailView.as_view(), name='movie-details')
 ]
