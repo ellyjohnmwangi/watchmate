52535c5969637e1f8811ebebea35cc85537e40d1
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-05-07 00:37:03 +0300

    created urls for new api views

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from watchlist_app.api.views import *
 
 urlpatterns = [
-    path('list/', MovieListView.as_view(), name='movie-list'),
-    path('<int:pk>', MovieDetailView.as_view(), name='movie-details')
+    path('list/', WatchlistView.as_view(), name='movie-list'),
+    path('platform/', StreamPlatformView.as_view(), name='platform-list'),
+    path('<int:pk>', WatchlistDetailView.as_view(), name='movie-details'),
+    path('platform/<int:pk>', StreamPlatformDetail.as_view(), name='platform-details')
+
 ]
