c372dc6717d9061d88268c4a2151ba86d687d87d
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-05-10 23:20:54 +0300

    Reviews api views url configured

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -3,8 +3,10 @@ from watchlist_app.api.views import *
 
 urlpatterns = [
     path('list/', WatchlistView.as_view(), name='movie-list'),
+    path('reviews/', ReviewView.as_view(), name='review-list'),
     path('platform/', StreamPlatformView.as_view(), name='platform-list'),
     path('<int:pk>', WatchlistDetailView.as_view(), name='movie-details'),
+    path('reviews/<int:pk>', ReviewDetailView.as_view(), name='review-details'),
     path('platform/<int:pk>', StreamPlatformDetail.as_view(), name='platform-details')
 
 ]
