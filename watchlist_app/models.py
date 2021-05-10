b82a81225ec87fe7041b02ed0f1ee68720f655fb
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-05-10 22:30:57 +0300

    created reviews model

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -14,9 +15,22 @@ class StreamPlatform(models.Model):
 class Watchlist(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
-    platform= models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name='watchlist')
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name='watchlist')
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.TextField(max_length=300)
+    watchlist = models.ForeignKey(Watchlist, on_delete=models.CASCADE, related_name='reviews')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.rating
+
