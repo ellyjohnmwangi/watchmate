0559b506c97b45bcd92a67f8cf466a7952ffb530
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-05-12 12:38:15 +0300

    created reviews create view.

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -6,9 +6,21 @@ from watchlist_app.api.serializers import *
 from rest_framework.response import Response
 
 
-class ReviewList(generics.ListCreateAPIView):
-    queryset = Review.objects.all()
+class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        watchlist = Watchlist.objects.get(pk=pk)
+        serializer.save(watchlist=watchlist)
+
+
+class ReviewList(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Review.objects.filter(watchlist=pk)
 
 
 class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -24,31 +36,6 @@ class StreamPlatformView(generics.ListCreateAPIView):
 class StreamPlatformDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = StreamPlatform.objects.all()
     serializer_class = StreamPlatformSerializer
-    def get(self, request, pk):
-        try:
-            platform = StreamPlatform.objects.get(pk=pk)
-        except StreamPlatform.DoesNotExist:
-            return Response({'Error': 'Platform not found!'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = StreamPlatformSerializer(platform, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        platform = StreamPlatform.objects.get(pk=pk)
-        serializer = StreamPlatformSerializer(platform, data=request.data, )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        try:
-            platform = StreamPlatform.objects.get(pk=pk)
-        except StreamPlatform.DoesNotExist:
-            return Response({'Error': 'Item Not Found'}, status=status.HTTP_404_NOT_FOUND)
-        platform.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class WatchlistView(generics.ListCreateAPIView):
