95a34cc11cdf75df9cadf10d91543b2895e63212
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-05-12 22:09:18 +0300

    user review validation added

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,5 +1,6 @@
-from rest_framework import status, mixins, generics
+from rest_framework import status, mixins, generics, viewsets
 # from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView
 from watchlist_app.models import StreamPlatform, Watchlist
 from watchlist_app.api.serializers import *
@@ -9,10 +10,20 @@ from rest_framework.response import Response
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = Watchlist.objects.get(pk=pk)
-        serializer.save(watchlist=watchlist)
+
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+
+        if review_queryset.exists():
+            raise ValidationError('Already Reviewed')
+
+        serializer.save(watchlist=watchlist, review_user=review_user)
 
 
 class ReviewList(generics.ListAPIView):
@@ -31,6 +42,15 @@ class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
 class StreamPlatformView(generics.ListCreateAPIView):
     queryset = StreamPlatform.objects.all()
     serializer_class = StreamPlatformSerializer
+
+    # def list(self, request):
+    #     queryset = StreamPlatform.objects.all()
+    #     serializer = StreamPlatformSerializer(queryset, many=True)
+    #     return Response(serializer.data)
+    #
+    # def retrieve(self, request):
+    #     queryset = StreamPlatform.objects.all()
+    #
 
 
 class StreamPlatformDetail(generics.RetrieveUpdateDestroyAPIView):
