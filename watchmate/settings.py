f5c6e816f18ff3d67e5b5a2661b2c30e5afe78dc
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-05-20 11:54:20 +0300

    user_app added to installed apps

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'watchlist_app',
+    'user_app',
     'rest_framework',
     'rest_framework.authtoken',
 ]
